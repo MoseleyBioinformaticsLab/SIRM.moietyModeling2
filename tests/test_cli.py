123da7ca69aaeb3ef75bf126e4157865bb4e8aff
Huan Jin <hji236@g.uky.edu>
2019-03-21 11:43:29 -0400

    Updated test_cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,8 +2,6 @@ import os
 import pytest
 
 
-
-
 def test_modeling():
 
     command1 = "python3 -m moiety_modeling modeling --models={0} --datasets={1} --optimizations={2} --repetition=100 --split --multiprocess --energyFunction=logDifference" \
@@ -31,15 +29,13 @@ def test_modeling():
     assert os.system(command4) == 0
 
 
-
-
 @pytest.mark.parametrize("optimizationPaths_txtfile", [
 
-
-
-
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/TNC_logDifference.txt"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/SLSQP_logDifference.txt"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/SAGA_1000_logDifference.txt"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/L_BFGS_B_logDifference.txt")
 ])
-
 def test_analysis_a(optimizationPaths_txtfile):
 
     command = "python3 -m moiety_modeling analyze optimizations --a {}".format(optimizationPaths_txtfile)
@@ -47,14 +43,13 @@ def test_analysis_a(optimizationPaths_txtfile):
     assert os.system(command) == 0
 
 
-
-
 @pytest.mark.parametrize("optimizationResults_jsonfile", [
 
-
-
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/SAGA_1000/best_model_SAGA_1000.json"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/SLSQP/best_model_SLSQP.json"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/TNC/best_model_TNC.json"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/L_BFGS_B/best_model_L_BFGS_B.json")
 ])
-
 def test_analysis_s(optimizationResults_jsonfile):
 
     command = "python3 -m moiety_modeling analyze optimizations --s {}".format(optimizationResults_jsonfile)
@@ -62,14 +57,13 @@ def test_analysis_s(optimizationResults_jsonfile):
     assert os.system(command) == 0
 
 
-
-
 @pytest.mark.parametrize("analysisPaths_txtfile", [
 
-
-
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/SAGA_1000_logDifference_analysis/SAGA_1000_logDifference_analysis.txt"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/TNC_logDifference_analysis/TNC_logDifference_analysis.txt"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/SLSQP_logDifference_analysis/SLSQP_logDifference_analysis.txt"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/L_BFGS_B_logDifference_analysis/L_BFGS_B_logDifference_analysis.txt")
 ])
-
 def test_rank(analysisPaths_txtfile):
 
     command = "python3 -m moiety_modeling analyze rank {} --rankCriteria=AICc".format(analysisPaths_txtfile)
@@ -77,12 +71,23 @@ def test_rank(analysisPaths_txtfile):
     assert os.system(command) == 0
 
 
+@pytest.mark.parameterize("analysisResults_jsonfile", [
 
+    ("/tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/SAGA_1000_logDifference_analysis/best_model_SAGA_1000_logDifference_Stats.json"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/TNC_logDifference_analysis/best_model_TNC_logDifference_Stats.json"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/SLSQP_logDifference_analysis/best_model_SLSQP_logDifference_Stats.json"),
+    ("tests/analysis_results/models_UDP_data_24_48_72_optimizationSetting_T_100_S_M_logDifference/L_BFGS_B_logDifference_analysis/best_model_L_BFGS_B_logDifference_Stats.json")
+])
+def test_plot(analysisResults_jsonfile):
 
-def test_table():
+    command = "python3 -m moiety_modeling plot moiety {}".format(analysisResults_jsonfile)
 
+    assert os.system(command) == 0
 
-def test_plot():
+    command1 = "python3 -m moiety_modeling plot isotopologue {}".format(analysisResults_jsonfile)
+
+    assert os.system(command1) == 0
+
 
 
 
