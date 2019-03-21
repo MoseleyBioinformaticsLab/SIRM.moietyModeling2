c53dfe873f344de277ba9e95c76d5c68f1888861
Huan Jin <hji236@g.uky.edu>
2019-03-21 15:35:00 -0400

    Updated optimization setting

--- tests/test_data/optimizationSetting.py
+++ tests/test_data/optimizationSetting.py
@@ -10,8 +10,8 @@ setting['TNC'] = {'methodParameters': None, 'optimizationSetting': 'TNC'}
 
 setting['SLSQP'] = {'methodParameters': None, 'optimizationSetting': 'SLSQP'}
 
-setting['SAGA'] = {'methodParameters': {'alpha': 1, 'crossoverRate': 0.05, 'mutationRate': 3, 'populationSize': 20, 'startTemperature': 0.5, 'stepNumber': 10000, 'temperatureStepSize': 100},
-        'noPrintAllResults': 1, 'noPrintBestResults': 1, 'optimizationSetting': 'SAGA_10000' }
+setting['SAGA'] = {'methodParameters': {'alpha': 1, 'crossoverRate': 0.05, 'mutationRate': 3, 'populationSize': 20, 'startTemperature': 0.5, 'stepNumber': 100, 'temperatureStepSize': 100},
+        'noPrintAllResults': 1, 'noPrintBestResults': 1, 'optimizationSetting': 'SAGA_100' }
 
 data ={'optimizations': setting}
 jsonpickle.set_encoder_options('json', sort_keys=True, indent=4)
