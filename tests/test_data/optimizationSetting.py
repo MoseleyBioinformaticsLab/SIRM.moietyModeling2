5f2fcdbdc763c1774452b248636a6ab2305790ee
Huan Jin <hji236@g.uky.edu>
2019-06-11 14:35:46 -0400

    Updated test optimization setting

--- tests/test_data/optimizationSetting.py
+++ tests/test_data/optimizationSetting.py
@@ -3,15 +3,18 @@
 import jsonpickle
 
 setting = {}
-
-# setting['L-BFGS-B'] = {'methodParameters': None, 'optimizationSetting': 'L_BFGS_B'}
-
-# setting['TNC'] = {'methodParameters': None, 'optimizationSetting': 'TNC'}
 #
-# setting['SLSQP'] = {'methodParameters': None, 'optimizationSetting': 'SLSQP'}
+# setting['L-BFGS-B'] = []
+# setting['L-BFGS-B'].append({'methodParameters': None, 'optimizationSetting': 'L_BFGS_B'})
+# setting['TNC'] = []
+# setting['TNC'].append({'methodParameters': None, 'optimizationSetting': 'TNC'})
 #
-setting['SAGA'] = {'methodParameters': {'alpha': 1, 'crossoverRate': 0.05, 'mutationRate': 3, 'populationSize': 20, 'startTemperature': 0.5, 'stepNumber': 100, 'temperatureStepSize': 100},
-        'noPrintAllResults': 1, 'noPrintBestResults': 1, 'optimizationSetting': 'SAGA_100' }
+# setting['SLSQP'] = []
+# setting['SLSQP'].append({'methodParameters': None, 'optimizationSetting': 'SLSQP'})
+#
+setting['SAGA'] = []
+setting['SAGA'].append({'methodParameters': {'alpha': 1, 'crossoverRate': 0.05, 'mutationRate': 3, 'populationSize': 20, 'startTemperature': 0.5, 'stepNumber': 100, 'temperatureStepSize': 100},
+        'noPrintAllResults': 1, 'noPrintBestResults': 1, 'optimizationSetting': 'SAGA_100' })
 
 data ={'optimizations': setting}
 jsonpickle.set_encoder_options('json', sort_keys=True, indent=4)
