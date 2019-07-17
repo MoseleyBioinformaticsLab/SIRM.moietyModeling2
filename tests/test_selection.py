c175fde83556ca85ec2cc58870f7579c95b82abf
Huan Jin <hji236@g.uky.edu>
2019-07-17 13:26:08 -0400

    Update test file

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -8,11 +8,11 @@ import math
     ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SAGA_1000/best_model_48h_72h_SAGA_1000.json",
      "tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SAGA_1000_logDifference_analysis/best_model_48h_72h_SAGA_1000_States.json"),
     ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SLSQP/best_model_48h_72h_SLSQP.json",
-     "tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SLSQP_logDifference_analysis/best_model_48h_72h_SLSQP_logDifference_Stats.json"),
+     "tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SLSQP_logDifference_analysis/best_model_48h_72h_SLSQP_States.json"),
     ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/TNC/best_model_48h_72h_TNC.json",
-     "tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/TNC_logDifference_analysis/best_model_48h_72h_TNC_logDifference_Stats.json"),
+     "tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/TNC_logDifference_analysis/best_model_48h_72h_TNC_States.json"),
     ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/L_BFGS_B/best_model_48h_72h_L_BFGS_B.json",
-     "tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/L_BFGS_B_logDifference_analysis/best_model_48h_72h_L_BFGS_B_logDifference_Stats.json")
+     "tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/L_BFGS_B_logDifference_analysis/best_model_48h_72h_L_BFGS_B_States.json")
 ])
 def test_analysis(optimization_file, analysis_file):
 
