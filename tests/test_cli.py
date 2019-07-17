691d53a473deec560a7ebd4f8b818dee92c3b984
Huan Jin <hji236@g.uky.edu>
2019-07-17 13:17:18 -0400

    Update test files

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -60,10 +60,10 @@ def test_analysis_a(optimizationPaths_txtfile):
 
 @pytest.mark.parametrize("optimizationResults_jsonfile", [
 
-    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SAGA_1000/best_model_SAGA_1000.json"),
-    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SLSQP/best_model_SLSQP.json"),
-    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/TNC/best_model_TNC.json"),
-    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/L_BFGS_B/best_model_L_BFGS_B.json")
+    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SAGA_1000/best_model_48h_72h_SAGA_1000.json"),
+    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SLSQP/best_model_48h_72h_SLSQP.json"),
+    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/TNC/best_model_48h_72h_TNC.json"),
+    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/L_BFGS_B/best_model_48h_72h_L_BFGS_B.json")
 ])
 def test_analysis_s(optimizationResults_jsonfile):
 
@@ -90,10 +90,10 @@ def test_rank(analysisPaths_txtfile):
 
 @pytest.mark.parametrize("analysisResults_jsonfile", [
 
-    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SAGA_1000_logDifference_analysis/best_model_SAGA_1000_logDifference_Stats.json"),
-    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/TNC_logDifference_analysis/best_model_TNC_logDifference_Stats.json"),
-    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SLSQP_logDifference_analysis/best_model_SLSQP_logDifference_Stats.json"),
-    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/L_BFGS_B_logDifference_analysis/best_model_L_BFGS_B_logDifference_Stats.json")
+    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SAGA_1000_logDifference_analysis/best_model_48h_72h_SAGA_1000_States.json"),
+    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/TNC_logDifference_analysis/best_model_48h_72h_TNC_States.json"),
+    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/SLSQP_logDifference_analysis/best_model_48h_72h_SLSQP_States.json"),
+    ("tests/analysis_results/all_single_models_UDP_data_48_72_optimization_setting_T_100_S_M_logDifference/L_BFGS_B_logDifference_analysis/best_model_48h_72h_L_BFGS_B_States.json")
 ])
 def test_plot(analysisResults_jsonfile):
 
